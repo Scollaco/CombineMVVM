ffa09170b799d1b464b2fde0330104e3ef32a28d
Saturnino Collaço <scollaco@gmail.com>
2020-03-21 21:26:49 -0400

    Initial Commit

---
+++ BadJokes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BadJokes
+//
+//  Created by Saturnino Collaco Teixeria Filho on 3/21/20.
+//  Copyright © 2020 Saturnino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
